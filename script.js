d5009042b0e954365061b8a72c202fb83dead0d8
rahatnabi <rahatnabi@ymail.com>
2021-02-14 19:46:44 +0600

    add svg showing support

--- script.js
+++ script.js
@@ -6,8 +6,36 @@ let btnNo = document.querySelector(".btn-no")
 btnYes.addEventListener("click", () => {
   //   document.querySelector(".love-you").classList.add("autoplay")
   document.querySelector(".love-you").play()
+  const el = document.querySelector(".love-svg")
+  el.classList.add("show-love-svg")
+
+  setTimeout(
+    function (el) {
+      //const el = document.getElementsByClassName("love-svg")[0]
+      el.style.display = "none"
+      el.style.animation = "none"
+      el.classList.remove("show-love-svg")
+      //el.offsetHeight /* trigger reflow */
+      el.style.animation = null
+    }.bind(this, el),
+    3300
+  )
 })
 
 btnNo.addEventListener("click", () => {
   document.querySelector(".fuck-you").play()
+  const el = document.querySelector(".fuck-svg")
+  el.classList.add("show-fuck-svg")
+
+  setTimeout(
+    function (el) {
+      //const el = document.getElementsByClassName("love-svg")[0]
+      el.style.display = "none"
+      el.style.animation = "none"
+      el.classList.remove("show-fuck-svg")
+      //el.offsetHeight /* trigger reflow */
+      el.style.animation = null
+    }.bind(this, el),
+    13300
+  )
 })
